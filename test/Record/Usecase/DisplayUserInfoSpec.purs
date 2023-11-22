85a456dfb4b12441b43f834eaa3c8f6b797f24fa
Pujo <pujoheadsoft@gmail.com>
2023-11-22 20:36:48 +0900

    wip

---
+++ test/Record/Usecase/DisplayUserInfoSpec.purs
@@ -0,0 +1,11 @@
+module Test.Record.Usecase.DisplayUserInfoSpec where
+
+import Prelude
+
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+spec = do
+  describe "" do
+    it "" do
+      "" `shouldEqual` ""
