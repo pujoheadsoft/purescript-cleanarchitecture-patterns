124d57c6a6629127a4ae7163acab40daa6c4f797
Pujo <pujoheadsoft@gmail.com>
2023-12-15 23:20:56 +0900

    wip

--- usecases/test/Test/Usecase/Main.purs
+++ usecases/test/Test/Usecase/Main.purs
@@ -3,10 +3,12 @@ module Test.Usecase.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff (launchAff_)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
+import Test.UseCase.SearchGitHubRepositoryUseCaseSpec (spec)
 
 main :: Effect Unit
-main = do
-  log "🍕"
-  log "You should add some tests."
+main = launchAff_ $ runSpec [consoleReporter] do
+  spec
 
