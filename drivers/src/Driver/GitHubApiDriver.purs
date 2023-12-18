2df1dab969240ded650ea0bf52ba82388e37bbcd
Pujo <pujoheadsoft@gmail.com>
2023-12-18 19:46:40 +0900

    do refactor

--- drivers/src/Driver/GitHubApiDriver.purs
+++ drivers/src/Driver/GitHubApiDriver.purs
@@ -2,11 +2,15 @@ module Driver.GitHubApiDriver where
 
 import Prelude
 
+import Affjax.RequestBody (json)
+import Affjax.RequestHeader (RequestHeader(..))
 import Affjax.ResponseFormat as ResponseFormat
-import Affjax.Web (get, printError)
+import Affjax.Web (defaultRequest, get, printError, request)
 import Data.Array (snoc)
 import Data.Either (Either(..), either)
+import Data.EtaConversionTransformer ((<<|))
 import Data.List.NonEmpty (NonEmptyList, foldl)
+import Data.MediaType (MediaType(..))
 import Data.String (joinWith)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (class MonadAff)
@@ -20,18 +24,24 @@ gitHubRepositoryGatewayPortFunction = { searchByName }
 searchByName :: forall m. MonadAff m => String -> m (Either ErrorMessage SearchResults)
 searchByName s = pure $ Right []
 
-request :: Aff (Either ErrorMessage SearchResults)
-request = do
-  get ResponseFormat.string "https://httpbin.org/ip" >>= either
+x :: String -> Aff (Either ErrorMessage SearchResults)
+x name = do
+  let
+    r = defaultRequest {
+      url = "https://api.github.com/search/repositories?q=" <> name <> "&language:purescript&sort=created&order=desc&page=1&per_page=10",
+      headers = [Accept $ MediaType "application/vnd.github+json"],
+      responseFormat = ResponseFormat.string
+    }
+  request r >>= either
     (pure <<< Left <<< printError)
-    \r -> toSearchResult r.body #
-      either
-        (\e -> pure <<< Left <<< joinWith "\n" $ renderForeignError <$> toArray e)
-        (pure <<< Right)
+    (either
+      (pure <<< Left <<< toErrorMessage)
+      (pure <<< Right)
+      <<< toSearchResult <<| _.body)
         
   where
-  toArray :: forall a. NonEmptyList a -> Array a
-  toArray = foldl snoc []
+  toErrorMessage :: MultipleErrors -> String
+  toErrorMessage e = joinWith "\n" $ renderForeignError <$> foldl snoc [] e
 
 toSearchResult :: String -> Either MultipleErrors SearchResults
 toSearchResult = readJSON
