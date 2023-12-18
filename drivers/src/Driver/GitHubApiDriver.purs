bb58f7b33f261b351b00eb77dac4157bbd885c60
Pujo <pujoheadsoft@gmail.com>
2023-12-18 19:58:12 +0900

    do refactor

--- drivers/src/Driver/GitHubApiDriver.purs
+++ drivers/src/Driver/GitHubApiDriver.purs
@@ -5,7 +5,7 @@ import Prelude
 import Affjax.RequestBody (json)
 import Affjax.RequestHeader (RequestHeader(..))
 import Affjax.ResponseFormat as ResponseFormat
-import Affjax.Web (defaultRequest, get, printError, request)
+import Affjax.Web (Request, defaultRequest, get, printError, request)
 import Data.Array (snoc)
 import Data.Either (Either(..), either)
 import Data.EtaConversionTransformer ((<<|))
@@ -23,22 +23,24 @@ gitHubRepositoryGatewayPortFunction = { searchByName }
 
 searchByName :: forall m. MonadAff m => String -> m (Either ErrorMessage SearchResults)
 searchByName name = do
-  let
-    r = defaultRequest {
-      url = "https://api.github.com/search/repositories?q=" <> name <> "&language:purescript&sort=created&order=desc&page=1&per_page=10",
-      headers = [Accept $ MediaType "application/vnd.github+json"],
-      responseFormat = ResponseFormat.string
-    }
-  liftAff $ request r >>= either
+  liftAff $ request (searchByNameRequest name) >>= either
     (pure <<< Left <<< printError)
     (either
       (pure <<< Left <<< toErrorMessage)
       (pure <<< Right)
       <<< toSearchResult <<| _.body)
-        
+
   where
   toErrorMessage :: MultipleErrors -> String
   toErrorMessage e = joinWith "\n" $ renderForeignError <$> foldl snoc [] e
+
+
+searchByNameRequest :: String -> Request String
+searchByNameRequest name = defaultRequest {
+  url = "https://api.github.com/search/repositories?q=" <> name <> "&language:purescript&sort=created&order=desc&page=1&per_page=10",
+  headers = [Accept $ MediaType "application/vnd.github+json"],
+  responseFormat = ResponseFormat.string
+}
 
 toSearchResult :: String -> Either MultipleErrors SearchResults
 toSearchResult = readJSON
